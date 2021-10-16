2342f3ed66a6fd5023b841a65acc50759bd17efc
Cheshta Sagar <cheshtas22@gmail.com>
2021-10-16 16:17:16 +0530

    fixed error related to uploading images

--- routes/upload.js
+++ routes/upload.js
@@ -22,14 +22,25 @@ router.post(
       { name: "file4", maxCount: 1 },
     ]),(req, res) => {
       
-       
+       let errors=[];
+
+       if (!req.files["file"] || !req.files["file1"]  || !req.files["file2"]  || !req.files["file3"] || !req.files["file4"] ){
+        errors.push({ msg: "Please upload all images" });
+      }
+      if (errors.length > 0) {
+        res.render("editProfile", {
+          errors, //    if entries are not according to validation render filled fields
+  
+        });
+      } else
+      
         User.findOneAndUpdate(
            {_id: req.user._id },{
-            "$set": { "displayPicture": req.files["file"][0].filename },
-            "$set": { "profilePictures": [ req.files["file1"][0].filename ,
+            "$set": { "displayPicture": req.files["file"][0].filename ,
+            "profilePictures": [ req.files["file1"][0].filename ,
             req.files["file2"][0].filename ,
             req.files["file3"][0].filename ,
-            req.files["file4"][0].filename ]}
+            req.files["file4"][0].filename ]},
             },
            function (err, docs) {
              if (err) {
@@ -38,11 +49,11 @@ router.post(
              else if (docs) {
                console.log("updated");
                req.flash("success_msg", "Profile Updated");
-               console.log(req.user._id);
+               console.log( req.files["file"][0].filename);
                res.redirect("/userProfile/editProfile");
              }
            });
-       
+          
  });
 
  module.exports = router;
