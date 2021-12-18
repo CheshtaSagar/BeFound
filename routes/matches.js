f52ad99107c574b50071942faa68620b66534804
Akanksha Verma <akanksha3454verma@gmail.com>
2021-12-19 01:25:07 +0530

    created newfeed card

---
+++ routes/matches.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+const auth = require('../config/auth');
+const isUser=auth.isUser;
+
+router.get("/find",async (req, res) => {
+    try {
+       var optresult = await User.findOne({ _id: req.user.id }).populate("matches");
+        console.log(optresult.matches);
+        console.log("hi");
+        res.status(200).json(optresult);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+  });
+
+
+
+module.exports = router;
--- routes/profile.js
+++ routes/profile.js
@@ -8,88 +8,108 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs"); //for storing encrypted password
 const passport = require("passport");
 const User = require("../models/User");
-const auth = require('../config/auth');
-const isUser=auth.isUser;
+const auth = require("../config/auth");
+const isUser = auth.isUser;
 var opttitle;
-
-
+var optresult=null;
 
 //profile
-router.get("/:opttitle",isUser, (req, res) => {
-    const loggedIn = req.isAuthenticated() ? true : false;
-  
-    let kmToRadian = function(miles){
-      var earthRadiusInMiles = 6378;
-      return miles / earthRadiusInMiles; //converting km to miles
-   };
-  
-   //using geowithin to find users that are within particular radius
-   var lg=req.user.location.coordinates[0];
-   var lt=req.user.location.coordinates[1];
-   opttitle=req.params.opttitle;
-   console.log(opttitle);
-   var popup=0;
-    var option = {
-       'location' : {
-        $geoWithin : {
-              $centerSphere :  [[lg ,lt ] , kmToRadian(req.user.radius)  ]
-      }
-      
-    }
-  };
-  
-    if(req.user.recommendedUsers.length == 0)//if we are fetching users based on location for the first time or
-                                            // have updated location 
-    {
-    User.find(option).then(data =>{
-      console.log("Visiting profile for the first time");
-  
-      User.findOneAndUpdate({_id: req.user._id}, {$set:{recommendedUsers: data }}, {new: true}, (err, doc) => {
-        if (err) {
-            console.log("Something went wrong when updating data!");
-        }
-          res.render("profile", {
-          user: req.user,
-          loggedIn: loggedIn,
-          recommendedUsers: data,//all users within radius are passed to ejs, 
-          popup: popup,           // where we are filtering based on preference.
-          likedUser: null,
-          opttitle: opttitle,                   
-          });
-        
-    });
-  
-        
-    });
-    }
-  
-    else
-    {
-        User.findOne({ _id: req.user.id }).populate("recommendedUsers").exec(
-        function (err, docs) {
-          if (err) {
-            console.log(err);
-            throw err;
-          }
-          else
-          {
-            console.log("Already visited profile before");
-            //console.log(docs);
-            res.render("profile", {
-              user: req.user,
-              loggedIn: loggedIn,
-              recommendedUsers: docs.recommendedUsers,//all users within radius are passed to ejs, 
-              popup: popup,// where we are filtering based on preference.
-              likedUser: null,  
-              opttitle: opttitle,                    
-              });
-          }
-    });
-  
-  }
-    
-  });
-  
+router.get("/:opttitle", isUser, async(req, res) => {
+  const loggedIn = req.isAuthenticated() ? true : false;
 
+  let kmToRadian = function (miles) {
+    var earthRadiusInMiles = 6378;
+    return miles / earthRadiusInMiles; //converting km to miles
+  };
+
+  opttitle = req.params.opttitle;
+  // console.log(opttitle);
+  // if(opttitle==='match'){
+  // try {
+  //   optresult = await User.findOne({ _id: req.user.id }).populate("matches");
+  //   console.log(optresult.matches);
+  //   console.log("hi");
+  // } catch (err) {
+  //   res.redirect("/login");
+  // }
+  // }
+
+
+  //using geowithin to find users that are within particular radius
+  var lg = req.user.location.coordinates[0];
+  var lt = req.user.location.coordinates[1];
+
+  var popup = 0;
+  var option = {
+    location: {
+      $geoWithin: {
+        $centerSphere: [[lg, lt], kmToRadian(req.user.radius)],
+      },
+    },
+  };
+
+  if (req.user.recommendedUsers.length == 0) {
+    //if we are fetching users based on location for the first time or
+    // have updated location
+    User.find(option).then((data) => {
+      console.log("Visiting profile for the first time");
+
+      User.findOneAndUpdate(
+        { _id: req.user._id },
+        { $set: { recommendedUsers: data } },
+        { new: true },
+        (err, doc) => {
+          if (err) {
+            console.log("Something went wrong when updating data!");
+          }
+          res.render("profile", {
+            user: req.user,
+            loggedIn: loggedIn,
+            recommendedUsers: data, //all users within radius are passed to ejs,
+            popup: popup, // where we are filtering based on preference.
+            likedUser: null,
+            opttitle: opttitle,
+            optresult: optresult,
+          });
+        }
+      );
+    });
+  } else {
+    User.findOne({ _id: req.user.id })
+      .populate("recommendedUsers")
+      .exec(function (err, docs) {
+        if (err) {
+          console.log(err);
+          throw err;
+        } else {
+          console.log("Already visited profile before");
+          //console.log(docs);
+          res.render("profile", {
+            user: req.user,
+            loggedIn: loggedIn,
+            recommendedUsers: docs.recommendedUsers, //all users within radius are passed to ejs,
+            popup: popup, // where we are filtering based on preference.
+            likedUser: null,
+            opttitle: opttitle,
+            optresult: optresult,
+          });
+        }
+      });
+  }
+});
+
+
+
+
+// router.get("/findmatch/matches",async (req, res) => {
+//   try {
+//      var optresult = await User.findOne({ _id: req.user.id }).populate("matches");
+//       console.log(optresult.matches);
+//       console.log("hi");
+//       res.status(200).json(optresult);
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+// });
 
 module.exports = router;
